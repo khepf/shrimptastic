4407b84d2d01f739b4a3d2715c343bc3cd0d5f70
Jeremy Kepf <jeremy1975@gmail.com>
2020-09-11 16:05:55 -0400

    add disclaimer

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -7,6 +7,10 @@ const Posts = ({posts, title}) => {
     <div className="posts-center">
       {/* posts column */ }
       <article>
+        <h5 className="posts-disclaimer"><em>Just to let you know, these posts contain affiliate links for which we may make a small 
+          commission at no extra cost to you should you make a purchase. We're passionate about music 
+          and Art in general. Our recommendations are genuine.</em></h5>
+          
         {posts.map(post => {
           return <Post key={post.id} {...post} />
         })}
